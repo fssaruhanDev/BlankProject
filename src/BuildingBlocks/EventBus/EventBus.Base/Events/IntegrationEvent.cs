da75e4f69c49112bbc0950544709df5cd0631521
fssaruhanDev <83281184+fssaruhanDev@users.noreply.github.com>
2024-08-03 13:43:20 +0300

    Add project files.

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Events/IntegrationEvent.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Base.Events
+{
+    public class IntegrationEvent
+    {
+
+        [JsonProperty]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        [JsonProperty]
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+
+
+    }
+}
---
+++ src/BuildingBlocks/EventBus/EventBus.Base/SubscriptionInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Base
+{
+    public class SubscriptionInfo
+    {
+        public Type HandlerType { get; set; }
+
+        public SubscriptionInfo(Type handleType)
+        {
+            HandlerType = handleType ?? throw new ArgumentNullException(nameof(handleType));    
+        }
+
+
+        public static SubscriptionInfo Typed(Type handleType)
+        {
+            return new SubscriptionInfo(handleType);
+        }
+    }
+}
