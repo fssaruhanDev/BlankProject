f26265abfe485010a54a733e85288b5d817dd07e
fssaruhanDev <83281184+fssaruhanDev@users.noreply.github.com>
2024-10-15 19:00:06 +0300

    aaa

--- src/Services/OrderService/OrderService.Api/Controllers/WeatherForecastController.cs
+++ src/Services/OrderService/OrderService.Api/Controllers/WeatherForecastController.cs
@@ -18,16 +18,19 @@ namespace OrderService.Api.Controllers
             _logger = logger;
         }
 
+        // hava durumlarýný getiren servis
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+        public IActionResult Get() {
+            return Ok("Hava durumu bilgileri");
         }
+        //Summeries bilgilerini döndüren servis
+        [HttpGet("Summeries", Name = "GetSummeries")]
+        public IActionResult GetSummeries()
+        {
+            return Ok(Summaries);
+        }
+
+
+
     }
 }
